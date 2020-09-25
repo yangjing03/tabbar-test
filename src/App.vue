68fde282b9717211e4b6ac54cc89db391e2f676a
yangjing <1136737152@qq.com>
2020-09-25 23:20:22 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
   <tabbar>
      <tabbaritem path="/home" activeColor="red">
            <template v-slot:icon-item>
- <img  src="./assets/img/zhuye.svg" alt="">
+ <img  src="assets/img/zhuye.svg" alt="">
            </template>
 
          <template v-slot:icon-item-1>
- <img  src="./assets/img/zhuye-1.svg" alt="">
+ <img  src="assets/img/zhuye-1.svg" alt="">
            </template>        <template v-slot:text-item>
             <div>
                 首页
@@ -17,10 +17,10 @@
         </tabbaritem>
             <tabbaritem  path="/category">
            <template v-slot:icon-item>
- <img src="./assets/img/leimupinleifenleileibie.svg" alt="">
+ <img src="assets/img/leimupinleifenleileibie.svg" alt="">
            </template>
                <template v-slot:icon-item-1>
- <img src="./assets/img/leimupinleifenleileibie-1.svg" alt="">
+ <img src="assets/img/leimupinleifenleileibie-1.svg" alt="">
            </template>
         <template v-slot:text-item>
             <div>
@@ -30,10 +30,10 @@
         </tabbaritem>
             <tabbaritem  path="/cart">
            <template v-slot:icon-item>
- <img src="./assets/img/gouwuche.svg" alt="">
+ <img src="assets/img/gouwuche.svg" alt="">
            </template>
                 <template v-slot:icon-item-1>
- <img src="./assets/img/gouwuche-1.svg" alt="">
+ <img src="assets/img/gouwuche-1.svg" alt="">
            </template>
         <template v-slot:text-item>
             <div>
@@ -43,10 +43,10 @@
         </tabbaritem>
             <tabbaritem  path="/profile">
            <template v-slot:icon-item>
- <img src="./assets/img/wode.svg" alt="">
+ <img src="assets/img/wode.svg" alt="">
            </template>
                 <template v-slot:icon-item-1>
- <img src="./assets/img/wode-1.svg" alt="">
+ <img src="assets/img/wode-1.svg" alt="">
            </template>
         <template v-slot:text-item>
             <div>
@@ -57,8 +57,8 @@
   </tabbar>
 </template>
 <script>
-import Tabbar from './components/Tabbar/Tabbar'
-import tabbaritem from './components/Tabbar/Tabbaritem'
+import Tabbar from '@/components/Tabbar/Tabbar'
+import tabbaritem from '@/components/Tabbar/Tabbaritem'
 export default {
   name: 'app',
   components: {
@@ -68,7 +68,7 @@ export default {
 }
 </script>
 <style>
-  @import url('./assets/css/base.css');
+  @import url('assets/css/base.css');
  
 </style>
 >
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -7,24 +7,25 @@ const routes = [
   },
   {
     path: '/home',
-    component: () => import('../views/home')
+    component: () => import('@/views/home')
   },
   {
     path: '/category',
-    component: () => import('../views/category')
+    component: () => import('@/views/category')
   },
   {
     path: '/cart',
-    component: () => import('../views/cart')
+    component: () => import('@/views/cart')
   },
   {
     path: '/profile',
-    component: () => import('../views/profile')
+    component: () => import('@/views/profile')
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
+  mode: 'history',
   routes
 })
 
