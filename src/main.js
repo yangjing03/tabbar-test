c680a2572489f4c75451c923e0992d12366fa584
yangjing <1136737152@qq.com>
2020-09-25 16:32:28 +0800

    项目创建

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import elementui from '../node_modules/vue-cli-plugin-element'
 
 createApp(App).use(router).mount('#app')
+App.use(elementui)
